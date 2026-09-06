20b717dbbd86c54e573180708cf3362b2d6764d2
Marcel Fontanillas <marcelfonta@gmail.com>
2026-09-06 19:56:24 +0200

    fix: força l'actualitzacio de la pwa

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'observatori-fontanillas-v22-31-0-frances-colaboracions-extrems-v3';
+const CACHE = 'observatori-fontanillas-v22-31-0-frances-colaboracions-extrems-v4';
 const API_CACHE = 'fontanilles-api-v2';
 const API_HOST = 'fonta-meteo.marcelfonta.workers.dev';
 const APP_SHELL = [
